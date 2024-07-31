550ba60e7d2e095dc44560fd74247c7643f1b75a
xongeeuse <xongeeuse@gmail.com>
2024-07-31 17:50:13 +0900

    240731_1

--- 1.hw/day03_list_2_1/21489_4837_sum_subset/21489_4837.py
+++ 1.hw/day03_list_2_1/21489_4837_sum_subset/21489_4837.py
@@ -6,11 +6,10 @@ A = list(range(1, 13))
 x = len(A)
 
 for testcase in range(1, T+1):
-    N, K = map(int, input().split())
+    N, K = map(int, input().split())    # N: 부분집합의 개수, K: 부분집합의 합
     cnt = 0
     for i in range(1 << x):     # 부분집합의 개수만큼 반복
         subset = []
-        # cnt = 0 여기 아님
         for j in range(x):      # 원소의 수만큼 비트를 비교
             if i & (1 << j):    # i의 j번 비트가 1이면
                 subset.append(A[j])     # j번 원소를 subset에 넣어라
